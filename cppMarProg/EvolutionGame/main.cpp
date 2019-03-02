d2d3aabafbffb8e3f8924bccc77d8f97a03385c8
nerzhul250 <gamersav1822@gmail.com>
2019-03-02 00:24:51 -0500

    up

--- cppMarProg/EvolutionGame/main.cpp
+++ cppMarProg/EvolutionGame/main.cpp
@@ -2,13 +2,34 @@
 
 using namespace std;
 
-//Cuerno - Ojo
-typedef pair<int,int> ii;
+int dp[5006], h[5006],w,n,maxi;
 
-bool adj[5005][5005];
-ii
+int L(int i){
+    if(dp[i]!=-1)return dp[i];
+    if(h[i]==maxi)return dp[i]=0;
+    for (int k=-w;k<w+1;k++){
+        if(k==0 || i+k<=0 || i+k>n || h[i+k]<=h[i]) continue;
+        dp[i]=max(dp[i],L(i+k));
+    }
+    dp[i]++;
+    return dp[i];
+}
 
 int main()
 {
-
+    while(scanf("%d %d",&n,&w)==2){
+        for (int i=1;i<=n;i++){
+            dp[i]=-1;
+        }
+        for (int i=1;i<=n;i++){
+            scanf("%d",&h[i]);
+            maxi=max(maxi,h[i]);
+        }
+        int maximo=-1;
+        for (int i=1;i<=n;i++){
+            L(i);
+            maximo=max(maximo,dp[i]);
+        }
+        printf("%d\n",maximo);
+    }
 }
---
+++ cppMarProg/SubsetFrequencies/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <unordered_set>
+#include <algorithm>
+#include <vector>
+
+
+using namespace std;
+
+typedef int64_t ll;
+
+int k,n;
+ll S[105],A[1000005],cuenta[105];
+unordered_set<ll>Aset;
+
+
+bool myfunction (pair<ll,ll> i,pair<ll,ll> j) {
+    if(i.second<j.second){
+        return false;
+    }
+    if(i.second>j.second){
+        return true;
+    }
+    if(i.second==j.second){
+        return i.first<j.first;
+    }
+}
+
+int main()
+{
+    scanf("%d %d",&n,&k);
+    for (int i=0;i<n;i++){
+        scanf("%lld",&A[i]);
+        Aset.insert(A[i]);
+    }
+    for (int i=0;i<k;i++){
+        scanf("%lld",&S[i]);
+    }
+
+    sort(A,A+n);
+    sort(S,S+k);
+
+    for (int j=0;j<k ;j++){
+        for (int i=0;i<n;i++){
+            if(S[j]-A[i]<=0)break;
+            if(S[j]-A[i]<=A[i])break;
+            if(Aset.find(S[j]-A[i])!=Aset.end()){
+                cuenta[j]++;
+            }
+        }
+    }
+
+    vector<pair<ll,ll>> answer;
+    for (int i=0;i<k;i++){
+        answer.push_back(pair<ll,ll>(S[i],cuenta[i]));
+    }
+    sort (answer.begin(), answer.end(), myfunction);
+    for (int i=0;i<k;i++){
+        printf("%lld %lld\n",answer[i].first,answer[i].second);
+    }
+    return 0;
+}
